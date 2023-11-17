333f54ca827b843c265ee2b58f0777d5f1108356
texan <alecksey1231@mail.ru>
2023-11-17 00:49:54 -0600

    index

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
         <h1 class="display-4">Congratulations!</h1>
 
-        <p class="lead">YEs</p>
+        <p class="lead">KU privaet</p>
 
         <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
     </div>
